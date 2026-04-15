8f9fbcbd194f45cc7416df512c6166d1ebc85d3f
AG-1laksh <resilient.warrior123@gmail.com>
2026-04-15 21:47:28 +0530

    UC12

--- App/src/TrainConsistManagementApp.java
+++ App/src/TrainConsistManagementApp.java
@@ -1,1 +1,13 @@
+import java.util.*;
 
+public class TrainConsistManagementApp {
+
+    // UC12: Safety validation
+    public boolean isTrainSafe(List<Bogie> bogies) {
+        return bogies.stream()
+                .allMatch(b ->
+                        !b.getType().equalsIgnoreCase("Cylindrical") ||
+                                b.getCargo().equalsIgnoreCase("Petroleum")
+                );
+    }
+}
