c148a23d158b32746c0790944355d768926b7f64
AG-1laksh <resilient.warrior123@gmail.com>
2026-04-17 14:30:10 +0530

    UC20

--- App/src/TrainConsistManagementApp.java
+++ App/src/TrainConsistManagementApp.java
@@ -1,1 +1,21 @@
 
+public class TrainConsistManagementApp {
+
+    // UC20: Linear Search with Exception Handling
+    public boolean searchBogie(String[] bogieIds, String key) {
+
+        // Fail-fast check
+        if (bogieIds == null || bogieIds.length == 0) {
+            throw new IllegalStateException("No bogies available for search");
+        }
+
+        // Normal search (linear)
+        for (String id : bogieIds) {
+            if (id.equals(key)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
