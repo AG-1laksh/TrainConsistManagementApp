2a78a4595af3b17eec0026b2bb165a9b8b948e3c
AG-1laksh <resilient.warrior123@gmail.com>
2026-04-17 14:11:56 +0530

    UC18

--- App/src/TrainConsistManagementApp.java
+++ App/src/TrainConsistManagementApp.java
@@ -1,1 +1,15 @@
 
+public class TrainConsistManagementApp {
+
+    // UC18: Linear Search
+    public boolean linearSearch(String[] bogieIds, String key) {
+
+        for (String id : bogieIds) {
+            if (id.equals(key)) {
+                return true; // found
+            }
+        }
+
+        return false; // not found
+    }
+}
--- App/test/TrainConsistManagementAppTest.java
+++ App/test/TrainConsistManagementAppTest.java
@@ -1,1 +1,48 @@
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TrainConsistManagementAppTest {
+
+    TrainConsistManagementApp app = new TrainConsistManagementApp();
+
+    // 1. Bogie found
+    @Test
+    void testSearch_BogieFound() {
+        String[] ids = {"BG101","BG205","BG309","BG412","BG550"};
+
+        assertTrue(app.linearSearch(ids, "BG309"));
+    }
+
+    // 2. Bogie not found
+    @Test
+    void testSearch_BogieNotFound() {
+        String[] ids = {"BG101","BG205","BG309","BG412","BG550"};
+
+        assertFalse(app.linearSearch(ids, "BG999"));
+    }
+
+    // 3. First element match
+    @Test
+    void testSearch_FirstElementMatch() {
+        String[] ids = {"BG101","BG205","BG309","BG412","BG550"};
+
+        assertTrue(app.linearSearch(ids, "BG101"));
+    }
+
+    // 4. Last element match
+    @Test
+    void testSearch_LastElementMatch() {
+        String[] ids = {"BG101","BG205","BG309","BG412","BG550"};
+
+        assertTrue(app.linearSearch(ids, "BG550"));
+    }
+
+    // 5. Single element array
+    @Test
+    void testSearch_SingleElementArray() {
+        String[] ids = {"BG101"};
+
+        assertTrue(app.linearSearch(ids, "BG101"));
+    }
+}
